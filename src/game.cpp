79b6f68be7e999403b06057458fc4f1c328a08cd
DelleVelleD <dellevelled@yahoo.com>
2022-05-13 11:12:01 -0400

    Alter the default board; Add ladders and falling

--- src/game.cpp
+++ src/game.cpp
@@ -51,12 +51,30 @@ void draw_board(){
 			
 			//draw tile background
 			switch(TileAt(x,y).bg){
-				case TileBG_Dirt:   { UI::RectFilled(tile_pos, tile_dims, color(70,37,33)); UI::Text(str8l("Dirt"),    tile_pos, UITextFlags_NoWrap); }break;
-				case TileBG_Surface:{ UI::Text(str8l("Surface"), tile_pos, UITextFlags_NoWrap); }break;
-				case TileBG_Sky:    { UI::Text(str8l("Sky"),     tile_pos, UITextFlags_NoWrap); }break;
-				case TileBG_Trench: { UI::RectFilled(tile_pos, tile_dims, color(150,112,91)); UI::Text(str8l("Trench"),  tile_pos, UITextFlags_NoWrap); }break;
-				case TileBG_Fort:   { UI::Text(str8l("Fort"),    tile_pos, UITextFlags_NoWrap); }break;
-				case TileBG_Tunnel: { UI::Text(str8l("Tunnel"),  tile_pos, UITextFlags_NoWrap); }break;
+				case TileBG_Dirt:{
+					UI::RectFilled(tile_pos, tile_dims, color(70,37,33));
+					UI::Text(str8l("Dirt"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;
+				case TileBG_Tunnel:{
+					UI::RectFilled(tile_pos, tile_dims, color(150,112,91));
+					UI::Text(str8l("Tunnel"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;
+				/*case TileBG_Trench:{
+					UI::Text(str8l("Trench"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;*/
+				case TileBG_BritishBase:{
+					UI::RectFilled(tile_pos, tile_dims, Color_Grey);
+					UI::RectFilled(tile_pos, tile_dims - tile_dims.yComp()*.75f, Color_DarkBlue);
+					UI::Text(str8l("Base (B)"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;
+				case TileBG_GermanBase:{
+					UI::RectFilled(tile_pos, tile_dims, Color_Grey);
+					UI::RectFilled(tile_pos, tile_dims - tile_dims.yComp()*.75f, Color_DarkRed);
+					UI::Text(str8l("Base (G)"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;
+				case TileBG_Sky:{
+					UI::Text(str8l("Sky"), tile_pos+vec2::ONE, UITextFlags_NoWrap);
+				}break;
 			}
 			
 			//draw foreground structures
@@ -98,12 +116,31 @@ void init_game(){
 	init_board(20, 10);
 	turn_count = 0;
 	
-	player0 = {0,             board_height-1, 3, TileFG_BritishPlayer, array<u32>(deshi_allocator)};
-	TileAt(player0.x,player0.y).bg = TileBG_Trench;
+	player0 = {1,             board_height-5, 3, TileFG_BritishPlayer, TileFG_BritishBomb,  array<u32>(deshi_allocator)};
+	TileAt(0,board_height-1).bg = TileBG_BritishBase;
+	for(int i = 1; i < board_width-1; ++i) TileAt(i,board_height-1).bg = TileBG_Sky;
+	TileAt(0,board_height-2).bg = TileBG_Tunnel;
+	TileAt(0,board_height-2).fg = TileFG_Ladder;
+	TileAt(0,board_height-3).bg = TileBG_Tunnel;
+	TileAt(0,board_height-3).fg = TileFG_Ladder;
+	TileAt(0,board_height-4).bg = TileBG_Tunnel;
+	TileAt(0,board_height-4).fg = TileFG_Ladder;
+	TileAt(0,board_height-5).bg = TileBG_Tunnel;
+	TileAt(0,board_height-5).fg = TileFG_Ladder;
+	TileAt(player0.x,player0.y).bg = TileBG_Tunnel;
 	TileAt(player0.x,player0.y).fg = TileFG_BritishPlayer;
 	
-	player1 = {board_width-1, board_height-1, 3, TileFG_GermanPlayer, array<u32>(deshi_allocator)};
-	TileAt(player1.x,player1.y).bg = TileBG_Trench;
+	player1 = {board_width-2, board_height-5, 3, TileFG_GermanPlayer,  TileFG_GermanBomb, array<u32>(deshi_allocator)};
+	TileAt(board_width-1,board_height-1).bg = TileBG_GermanBase;
+	TileAt(board_width-1,board_height-2).bg = TileBG_Tunnel;
+	TileAt(board_width-1,board_height-2).fg = TileFG_Ladder;
+	TileAt(board_width-1,board_height-3).bg = TileBG_Tunnel;
+	TileAt(board_width-1,board_height-3).fg = TileFG_Ladder;
+	TileAt(board_width-1,board_height-4).bg = TileBG_Tunnel;
+	TileAt(board_width-1,board_height-4).fg = TileFG_Ladder;
+	TileAt(board_width-1,board_height-5).bg = TileBG_Tunnel;
+	TileAt(board_width-1,board_height-5).fg = TileFG_Ladder;
+	TileAt(player1.x,player1.y).bg = TileBG_Tunnel;
 	TileAt(player1.x,player1.y).fg = TileFG_GermanPlayer;
 	
 	last_action = Message_None;
@@ -119,6 +156,11 @@ void update_game(){
 #define TileAtOtherPlayer() TileAt(player->x,player->y)
 #define TileNearOtherPlayer(_x,_y) TileAt(player->x+_x,player->y+_y)
 	
+	//debug
+	if(key_pressed(Key_ENTER | InputMod_AnyShift)){
+		turn_count += 1;
+	}
+	
 	u32 action_performed = Message_None;
 	if(turn_count % 2 == player_idx){
 		player = (player_idx) ? &player1 : &player0;
@@ -126,46 +168,55 @@ void update_game(){
 		
 		//// moving input ////
 		if      (   key_pressed(Key_UP    | InputMod_None)
-				 && player->y < board_height-1 && TileNearPlayer(0, 1).bg == TileBG_Trench){
+				 && player->y < board_height-1 && TileNearPlayer(0, 1).bg == TileBG_Tunnel
+				 && HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
 			action_performed = Message_MoveUp;
 		}else if(   key_pressed(Key_DOWN  | InputMod_None)
-				 && player->y > 0              && TileNearPlayer(0,-1).bg == TileBG_Trench){
+				 && player->y > 0              && TileNearPlayer(0,-1).bg == TileBG_Tunnel
+				 && HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
 			action_performed = Message_MoveDown;
 		}else if(   key_pressed(Key_RIGHT | InputMod_None)
-				 && player->x < board_width-1  && TileNearPlayer( 1,0).bg == TileBG_Trench){
+				 && player->x < board_width-1  && TileNearPlayer( 1,0).bg == TileBG_Tunnel){
 			action_performed = Message_MoveRight;
 		}else if(   key_pressed(Key_LEFT  | InputMod_None)
-				 && player->x > 0              && TileNearPlayer(-1,0).bg == TileBG_Trench){
+				 && player->x > 0              && TileNearPlayer(-1,0).bg == TileBG_Tunnel){
 			action_performed = Message_MoveLeft;
 		}
 		
 		//// digging input ////
 		if      (   key_pressed(Key_UP    | InputMod_AnyShift)
-				 && player->y < board_height-1 && TileNearPlayer(0,1).bg == TileBG_Dirt){
+				 && player->y < board_height-1 && TileNearPlayer(0, 1).bg == TileBG_Dirt){
 			action_performed = Message_DigUp;
 		}else if(   key_pressed(Key_DOWN  | InputMod_AnyShift)
-				 && player->y > 0 && TileNearPlayer(0,-1).bg == TileBG_Dirt){
+				 && player->y > 0              && TileNearPlayer(0,-1).bg == TileBG_Dirt){
 			action_performed = Message_DigDown;
 		}else if(   key_pressed(Key_RIGHT | InputMod_AnyShift)
-				 && player->x < board_width-1 && TileNearPlayer(1,0).bg == TileBG_Dirt){
+				 && player->x < board_width-1  && TileNearPlayer( 1,0).bg == TileBG_Dirt){
 			action_performed = Message_DigRight;
 		}else if(   key_pressed(Key_LEFT  | InputMod_AnyShift)
-				 && player->x > 0 && TileNearPlayer(-1,0).bg == TileBG_Dirt){
+				 && player->x > 0              && TileNearPlayer(-1,0).bg == TileBG_Dirt){
 			action_performed = Message_DigLeft;
 		}
 		
 		//// building input ////
-		//TODO building
+		if      (   key_pressed(Key_L | InputMod_None)
+				 && TileAtPlayer().bg == TileBG_Tunnel
+				 && !HasFlag(TileAtPlayer().fg, TileFG_Pillar)){
+			action_performed = Message_BuildLadder;
+		}else if(   key_pressed(Key_P | InputMod_None)
+				 && TileAtPlayer().bg == TileBG_Tunnel
+				 && !HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
+			action_performed = Message_BuildPillar;
+		}
 		
 		//// bombing input ////
-		if      (key_pressed(Key_B    | InputMod_None)){   //place bomb
-			if(player->bombs > 0){
-				action_performed = Message_PlaceBomb;
-			}
-		}else if(key_pressed(Key_B  | InputMod_AnyShift)){ //detonate bomb
-			if(player->placed_bombs.count > 0){
-				action_performed = Message_DetonateBomb;
-			}
+		if      (   key_pressed(Key_B | InputMod_None)
+				 && !HasFlag(TileAtPlayer().fg, TileFG_BritishBomb|TileFG_GermanBomb)
+				 && player->bombs > 0){
+			action_performed = Message_PlaceBomb;
+		}else if(   key_pressed(Key_B | InputMod_AnyShift)
+				 && player->placed_bombs.count > 0){
+			action_performed = Message_DetonateBomb;
 		}
 		
 		if(action_performed <= Message_MOVES_END && action_performed >= Message_MOVES_START){
@@ -174,9 +225,9 @@ void update_game(){
 			last_action_y = player->y;
 			
 			turn_count += 1;
-			turn_info.uid = player_idx;
-			turn_info.x = player->x;
-			turn_info.y = player->y;
+			turn_info.uid     = player_idx;
+			turn_info.x       = player->x;
+			turn_info.y       = player->y;
 			turn_info.message = action_performed;
 			net_client_send(turn_info);
 		}
@@ -203,59 +254,75 @@ void update_game(){
 	switch(action_performed){
 		//// moving actions ////
 		case Message_MoveUp:{
-			RemoveFlag(TileAtPlayer().fg, player->flag);
+			RemoveFlag(TileAtPlayer().fg, player->player_flag);
 			player->y += 1;
-			AddFlag(TileAtPlayer().fg, player->flag);
+			AddFlag(TileAtPlayer().fg, player->player_flag);
 		}break;
 		case Message_MoveDown:{
-			RemoveFlag(TileAtPlayer().fg, player->flag);
+			RemoveFlag(TileAtPlayer().fg, player->player_flag);
 			player->y -= 1;
-			AddFlag(TileAtPlayer().fg, player->flag);
+			AddFlag(TileAtPlayer().fg, player->player_flag);
 		}break;
 		case Message_MoveRight:{
-			RemoveFlag(TileAtPlayer().fg, player->flag);
+			RemoveFlag(TileAtPlayer().fg, player->player_flag);
 			player->x += 1;
-			AddFlag(TileAtPlayer().fg, player->flag);
+			while(   TileNearPlayer(0,-1).bg == TileBG_Tunnel
+				  && !HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
+				player->y -= 1;
+			}
+			AddFlag(TileAtPlayer().fg, player->player_flag);
 		}break;
 		case Message_MoveLeft:{
-			RemoveFlag(TileAtPlayer().fg, player->flag);
+			RemoveFlag(TileAtPlayer().fg, player->player_flag);
 			player->x -= 1;
-			AddFlag(TileAtPlayer().fg, player->flag);
+			while(   TileNearPlayer(0,-1).bg == TileBG_Tunnel
+				  && !HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
+				player->y -= 1;
+			}
+			AddFlag(TileAtPlayer().fg, player->player_flag);
 		}break;
 		
 		//// digging actions ////
 		case Message_DigUp:{
-			TileNearPlayer(0, 1).bg = TileBG_Trench;
+			TileNearPlayer(0, 1).bg = TileBG_Tunnel;
 		}break;
 		case Message_DigDown:{
-			TileNearPlayer(0,-1).bg = TileBG_Trench;
+			TileNearPlayer(0,-1).bg = TileBG_Tunnel;
+			RemoveFlag(TileAtPlayer().fg, player->player_flag);
+			while(   TileNearPlayer(0,-1).bg == TileBG_Tunnel
+				  && !HasFlag(TileAtPlayer().fg, TileFG_Ladder)){
+				player->y -= 1;
+			}
+			AddFlag(TileAtPlayer().fg, player->player_flag);
 		}break;
 		case Message_DigRight:{
-			TileNearPlayer( 1,0).bg = TileBG_Trench;
+			TileNearPlayer( 1,0).bg = TileBG_Tunnel;
 		}break;
 		case Message_DigLeft:{
-			TileNearPlayer(-1,0).bg = TileBG_Trench;
+			TileNearPlayer(-1,0).bg = TileBG_Tunnel;
 		}break;
 		
 		//// building actions ////
 		case Message_BuildLadder:{
-			
+			AddFlag(TileAtPlayer().fg, TileFG_Ladder);
 		}break;
 		case Message_BuildPillar:{
-			
+			AddFlag(TileAtPlayer().fg, TileFG_Pillar);
 		}break;
 		
 		//// bombing actions ////
 		case Message_PlaceBomb:{
 			player->placed_bombs.add(ToLinear(player->x,player->y));
 			player->bombs -= 1;
+			AddFlag(TileAtPlayer().fg, player->bomb_flag);
 		}break;
 		case Message_DetonateBomb:{
 			forI(player->placed_bombs.count){
 				//explode up
 				s32 up_tile = player->placed_bombs[i] - board_width;
 				if(up_tile >= 0 && up_tile < board_height){
-					TileAtLinear(up_tile).bg = TileBG_Trench;
+					TileAtLinear(up_tile).bg = TileBG_Tunnel;
+					TileAtLinear(up_tile).fg = TileFG_None;
 					if(ToLinear(player->x,player->y) == up_tile){
 						//TODO player dies
 					}
@@ -267,7 +334,8 @@ void update_game(){
 				//explode down
 				s32 down_tile = player->placed_bombs[i] + board_width;
 				if(down_tile >= 0 && down_tile < board_height){
-					TileAtLinear(down_tile).bg = TileBG_Trench;
+					TileAtLinear(down_tile).bg = TileBG_Tunnel;
+					TileAtLinear(down_tile).fg = TileFG_None;
 					if(ToLinear(player->x,player->y) == down_tile){
 						//TODO player dies
 					}
@@ -279,7 +347,8 @@ void update_game(){
 				//explode right
 				s32 right_tile = player->placed_bombs[i] + 1;
 				if(right_tile >= 0 && right_tile < board_height){
-					TileAtLinear(right_tile).bg = TileBG_Trench;
+					TileAtLinear(right_tile).bg = TileBG_Tunnel;
+					TileAtLinear(right_tile).fg = TileFG_None;
 					if(ToLinear(player->x,player->y) == right_tile){
 						//TODO player dies
 					}
@@ -291,7 +360,8 @@ void update_game(){
 				//explode left
 				s32 left_tile = player->placed_bombs[i] - 1;
 				if(left_tile >= 0 && left_tile < board_height){
-					TileAtLinear(left_tile).bg = TileBG_Trench;
+					TileAtLinear(left_tile).bg = TileBG_Tunnel;
+					TileAtLinear(left_tile).fg = TileFG_None;
 					if(ToLinear(player->x,player->y) == left_tile){
 						//TODO player dies
 					}
@@ -307,11 +377,6 @@ void update_game(){
 			deinit_board();
 			game_active = 0;
 		}break;
-	}
-	
-	//debug
-	if(key_pressed(Key_ENTER | InputMod_AnyShift)){
-		turn_count += 1;
 	}
 	
 	//draw things
